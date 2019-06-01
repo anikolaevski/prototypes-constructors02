d31dffcd9806d9da8854c2aace8e02cb8f1f2fe4
anikolaevski <anikolaevski@gmail.com>
2019-06-02 00:45:41 +0300

    test corrected

--- test/Character.test.js
+++ test/Character.test.js
@@ -16,18 +16,24 @@ test('Test Bowman', () => {
 
 test('Test long name', () => {
   const result = new Character('Какой-то шушпанцер', 'Bowman');
-  const expected = {};
+  const expected = {
+    error: 'Введено имя "Какой-то шушпанцер". Должно быть: строка, min 2 символа, max 10',
+  };
   expect(result).toEqual(expected);
 });
 
 test('Test short name', () => {
   const result = new Character('A', 'Bowman');
-  const expected = {};
+  const expected = {
+    error: 'Введено имя "A". Должно быть: строка, min 2 символа, max 10',
+  };
   expect(result).toEqual(expected);
 });
 
 test('Test wrong type', () => {
   const result = new Character('Персонаж', 'Непонятно что');
-  const expected = {};
+  const expected = {
+    error: 'Введен тип: "Непонятно что". type - один из типов:  Zombie Undead Daemon Magician Swordsman Bowman',
+  };
   expect(result).toEqual(expected);
 });
